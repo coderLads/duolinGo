ecbb98848d9a60dc016a403bb7edc03e6919d3eb
Milo Rupp <badtzmaru139@gmail.com>
2018-09-25 13:00:31 -0700

    First commit with basic window

---
+++ main.js
@@ -0,0 +1,43 @@
+const {
+    app,
+    BrowserWindow
+} = require('electron')
+
+let mainWindow
+
+function createWindow() {
+
+    // Create the browser window.
+    mainWindow = new BrowserWindow({
+        width: 690,
+        height: 800,
+        title: "duolinGo",
+    })
+
+    // remove meus
+    mainWindow.setMenu(null);
+
+    // and load the index.html of the app.
+    mainWindow.loadFile('index.html')
+
+    // Open the DevTools.
+    // mainWindow.webContents.openDevTools()
+
+    // Emitted when the window is closed.
+    mainWindow.on('closed', function () {
+        mainWindow = null
+    })
+}
+app.on('ready', createWindow)
+
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+app.on('activate', function () {
+    if (mainWindow === null) {
+        createWindow()
+    }
+})
