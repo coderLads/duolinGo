fb2b55238dff112b60fb2999e62451c0e334457a
Milo Rupp <badtzmaru139@gmail.com>
2018-09-25 17:37:57 -0700

    clean up code

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
 const {
     app,
     BrowserWindow,
-    Menu,
-    globalShortcut
 } = require('electron');
 
-const {Client} = require('discord-rpc');
-const rpc      = new Client({transport: 'ipc'});
+const {
+    Client
+} = require('discord-rpc');
+
+const rpc = new Client({
+    transport: 'ipc'
+});
 
 clientId = '494272947788316672';
 
 let mainWindow,
     WindowSettings = {
         backgroundColor: '#FFF',
-        useContentSize: false,
-       // autoHideMenuBar: false,
+        width: 690,
+        height: 800,
         resizable: true,
-        center: true,
-        frame: true,
-        alwaysOnTop: false,
         title: 'duolinGo',
         icon: __dirname + '/build/logo.ico',
         webPreferences: {
@@ -27,53 +27,62 @@ let mainWindow,
         },
     },
     login = (tries = 0) => {
-        rpc.login({ clientId }).catch(console.error);
-    },
-    getInfos = `(function() {
-        
-        if (true) {
-            return {
-                songName: 'uyeee',
-            }
-        }
+        rpc.login({
+            clientId
+        }).catch(console.error);
+    };
 
-    })()`;
+async function fetchActivity() {
 
-async function checkSoundCloud() {
-
-    // rpc.setActivity({
-    //     details: 'ye',
-    //     state: 'some weeb stuff'
-    // });
-    
     if (!rpc || !mainWindow) return;
-    
 
-    let infos = await mainWindow.webContents.executeJavaScript(getInfos);
-    
+    let infos = await mainWindow.webContents.executeJavaScript(
+        `(function() {
+            if (true) {
+                return {
+                    activity: 'uyeee',
+                }
+            }
+        })()`
+    );
+
 
     if (infos) { // if !infos don't change presence then.
-        let {songName, author, length, timePassed} = infos;
+        let {
+            activity,
+            author,
+            length,
+            timePassed
+        } = infos;
 
         rpc.setActivity({
-            details: songName,
+            details: activity,
         });
     }
 }
 
+// app ready
 app.on('ready', () => {
     mainWindow = new BrowserWindow(WindowSettings);
-    mainWindow.maximize();
+    mainWindow.setMenu(null);
     mainWindow.loadURL("http://www.duolingo.com/");
     mainWindow.webContents.openDevTools();
-    
+
+    // setup event handler for when the site finishes loading
+    mainWindow.webContents.on('did-finish-load', () => {
+        mainWindow.webContents.insertCSS('::-webkit-scrollbar { width: 0px; height: 0px; background: transparent;}');
+    });
+
     login();
+
+
 });
 
+// rpc ready
 rpc.on('ready', () => {
-    checkSoundCloud();
+    fetchActivity();
     setInterval(() => {
-        checkSoundCloud();
+        fetchActivity();
     }, 5E3);
 });
 
